367259c8dcbc746a2717dacfaa25ead57b82bd25
Nicholas Stephen <git@nickstephen.com>
2014-02-16 14:36:16 +1100

    Moved ListFragment extensions into HoloEverywhere from Android library

--- library/src/org/holoeverywhere/app/ListFragment.java
+++ library/src/org/holoeverywhere/app/ListFragment.java
@@ -37,6 +37,8 @@ public class ListFragment extends Fragment {
         }
     };
     private TextView mStandardEmptyView;
+    private boolean mIsFocused = false;
+    private boolean mIsExiting = false;
 
     private void ensureList() {
         if (mList != null) {
@@ -213,5 +215,28 @@ public class ListFragment extends Fragment {
     public void setSelection(int position) {
         ensureList();
         mList.setSelection(position);
+    }
+
+    protected final void setFocused(boolean val) {
+        mIsFocused = val;
+    }
+
+    public final boolean isFocused() {
+        return mIsFocused;
+    }
+
+    public final boolean isExiting() {
+        return mIsExiting;
+    }
+
+    public void popFragment() {
+        mIsExiting = true;
+        this.getFragmentManager().popBackStack();
+
+        onFragmentPopped();
+    }
+
+    protected void onFragmentPopped() {
+        mIsFocused = false;
     }
 }
